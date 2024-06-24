110bbfbed0bdce627f52df027c2d2db63e5c4baf
Calebe Oliveira <calebe81899026@gmail.com>
2024-06-24 12:02:16 -0300

    add commponents, constants and theme

---
+++ src/components/board.tsx
@@ -0,0 +1,44 @@
+import { StyleSheet, View } from "react-native";
+import { BORDER, BORDER_RADIUS, GAP, HEADER_HEIGHT, PIXEL } from "../constants";
+import { colors } from "../styles/theme";
+
+type BoardProps = {
+    rows: number,
+    cols: number,
+    top: number
+}
+
+export default function Board({rows, cols, top}: BoardProps){
+
+    const dots = Array(rows * cols).fill(0)
+
+    return(
+        <View style={[styles.board, { top: HEADER_HEIGHT + top }]}>
+            {
+                dots.map((_,index) => (
+                    <View style={styles.pixel} key={index}></View>
+                ))
+            }
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    board: {
+        display: "flex",
+        flexDirection: "row",
+        flexWrap: "wrap",
+        position: "absolute",
+        marginHorizontal: BORDER
+    },
+    pixel: {
+        width: PIXEL,
+        height: PIXEL,
+        borderColor: colors.p6,
+        borderWidth: GAP,
+        borderRadius: BORDER_RADIUS,
+        backgroundColor: colors.p5,
+        justifyContent: "center",
+        alignItems: "center"
+    }
+})
